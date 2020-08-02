90d79a126076b132989cbbdd761679b2486d5d8c
Paul Bradley <bradleypauld@gmail.com>
2020-08-01 21:53:27 -0500

    update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,14 +8,6 @@ const FILES_TO_CACHE = [
   './js/index.js',
   './js/idb.js',
   './manifest.json',
-  './icons/icon-512x512.png',
-  './icons/icon-384x384.png',
-  './icons/icon-192x192.png',
-  './icons/icon-152x152.png',
-  './icons/icon-144x144.png',
-  './icons/icon-128x128.png',
-  './icons/icon-96x96.png',
-  './icons/icon-72x72.png'
 ];
  
 self.addEventListener('install', function (e) {
